57d340c4ae470573e682ee5b80b8c31ea5369e1f
Maycon Jardim <mayconsmill@gmail.com>
2022-06-27 21:10:09 -0300

    Adicionando informações extras no token

---
+++ src/main/java/com/nbaliga/components/JwtTokenEnhancer.java
@@ -0,0 +1,35 @@
+package com.nbaliga.components;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import com.nbaliga.entities.User;
+import com.nbaliga.repositories.UserRepository;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		User user = userRepository.findByName(authentication.getName());
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("userName", user.getName());
+		map.put("userTeam", user.getTeam().getName());
+		
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+		token.setAdditionalInformation(map);
+		return token;
+	}
+
+}
