ee54340e0d334a9f0914d5bd0d00f5eba528c6e7
Maycon Jardim <mayconsmill@gmail.com>
2022-10-14 20:32:51 -0300

    Projeto criado

---
+++ src/test/java/com/nbaligabackend/NbaligaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbaligabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaligaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
